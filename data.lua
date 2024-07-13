3ad40b95467fbf1c71388583737ad157c959153d
Neer <legitting@gmail.com>
2024-07-13 16:49:13 +0200

    Initial Version

---
+++ data.lua
@@ -0,0 +1,34 @@
+local data = {}
+
+data.rarities = {
+    "Common",
+    "Magic",
+    "Magic_2",
+    "Rare",
+    "Rare_2",
+    "Legendary",
+    "Unique",
+}
+
+data.custom_items = {
+    boss_items = {
+        [1502128] = "Living Steel",
+        [1518053] = "Distilled Fear",
+        [1522891] = "Exquisite Blood",
+        [1524924] = "Shard of Agony",
+        [1489420] = "Malignant Heart",
+        [1489418] = "Gurgling Head",
+        [1850845] = "Stygian Stone",
+        [1524917] = "Mucus-Slick Egg",
+        [1810144] = "Sandscorched Shackles",
+        [1812685] = "Pincushioned Doll",
+        [1489422] = "Blackened Femur",
+        [1882910] = "Profane Mindcage",
+    },
+    elixirs = {
+        [1856350] = "Elixir of Holy Bolts",
+        [1856252] = "Elixir of Momentum",
+    }
+}
+
+return data
---
+++ gui.lua
@@ -0,0 +1,57 @@
+local plugin_label = "autolooteer"
+local gui = {}
+local data = require("data")
+
+gui.elements = {
+    main_tree = tree_node:new(0),
+    main_toggle = checkbox:new(false, get_hash(plugin_label .. "_main_toggle")),
+    behavior_combo = combo_box:new(0, get_hash(plugin_label .. "_behavior_combo")),
+    rarity_combo = combo_box:new(0, get_hash(plugin_label .. "_rarity_combo")),
+    choices_tree = tree_node:new(1),
+    distance_slider = slider_int:new(1, 5, 2, get_hash(plugin_label .. "_distance_slider")),
+    greater_affix_slider = slider_int:new(0, 3, 0, get_hash(plugin_label .. "_greater_affix_slider")),
+    unique_greater_affix_slider = slider_int:new(0, 3, 0, get_hash(plugin_label .. "_unique_greater_affix_slider")),
+    quest_items_toggle = checkbox:new(false, get_hash(plugin_label .. "_quest_items_toggle")),
+    boss_items_toggle = checkbox:new(false, get_hash(plugin_label .. "_boss_items_toggle")),
+    elixir_items_toggle = checkbox:new(false, get_hash(plugin_label .. "_elixir_items_toggle")),
+    draw_wanted_toggle = checkbox:new(false, get_hash(plugin_label .. "_draw_wanted_toggle")),
+}
+
+gui.behaviors = {
+    "Always",
+    "Orbwalk"
+}
+
+function gui:render()
+    if not gui.elements.main_tree:push("Autolooteer") then
+        return
+    end
+
+    gui.elements.main_toggle:render("Enable", "Toggles the main module on/off")
+    -- Let's not continue rendering options when its disabled :D
+    if not gui.elements.main_toggle:get() then
+        gui.elements.main_tree:pop()
+        return
+    end
+
+    gui.elements.behavior_combo:render("Behavior", gui.behaviors, "When do you want the autolooter to execute?")
+    gui.elements.rarity_combo:render("Rarity", data.rarities, "Minimum Rarity for bot to consider picking up.")
+
+    if gui.elements.choices_tree:push("Settings") then
+        gui.elements.distance_slider:render("Distance", "Distance from the loot to execute pickup")
+        gui.elements.greater_affix_slider:render("Legendary GA Count", "Minimum GA's to consider picking up legendary")
+        gui.elements.unique_greater_affix_slider:render("Unique GA Count", "Minimum GA's to consider picking up unique")
+        gui.elements.quest_items_toggle:render("Quest Items",
+            "Do you want to pickup Quest items, this includes Objectives in dungeons.")
+        gui.elements.boss_items_toggle:render("Boss Items", "Do you want to pickup Boss summon items?")
+        gui.elements.elixir_items_toggle:render("Rare Elixirs",
+            "Do you wanna pickup Rare Elixirs? (Momentum, Holy Bolts)")
+        gui.elements.draw_wanted_toggle:render("Draw Wanted",
+            "Do you want to draw the items that the bot considers picking up? (Debug)")
+        gui.elements.choices_tree:pop()
+    end
+
+    gui.elements.main_tree:pop()
+end
+
+return gui
